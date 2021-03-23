ed487592192f9f43bf6501c75359b76202db8e33
Hassan Ali Arshad <hassanali.arshad98@users.noreply.github.com>
2021-03-24 02:41:09 +0500

    Update AppReducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,13 +1,13 @@
 const AppReducer = (state, action) => {
 	switch (action.type) {
-		case "Delete Transaction":
+		case "DELETE":
 			return {
 				...state,
 				transactions: state.transactions.filter(
 					(transaction) => transaction.id !== action.payload
 				),
 			};
-		case "Add transaction":
+		case "ADD":
 			return {
 				...state,
 				transactions: [action.payload, ...state.transactions],
